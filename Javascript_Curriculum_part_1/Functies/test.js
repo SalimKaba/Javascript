ab0e943298817ef40f56924165fd8a1d7bc2470c
Salim <salimm.kaba@gmail.com>
2018-12-10 09:08:44 +0100

    1e

---
+++ Javascript_Curriculum_part_1/Functies/test.js
@@ -0,0 +1,10 @@
+document.write("<h1>Oefeningen: Functies<br></h1>");
+var add = function(a, b) {
+return a + b;
+}
+let c = parseInt(prompt("geef 1e getal"));
+let d = parseInt(prompt("geef 2e getal"));
+
+
+
+console.log(add(c,d));
